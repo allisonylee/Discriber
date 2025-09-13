bede9b6637b17721d9e432b288a86e82b2ffcfc5
Arvin M <arvinmefsha1@gmail.com>
2025-09-13 17:46:27 -0400

    speaker distinguisher and gemini api

--- assemblyai.js
+++ assemblyai.js
@@ -1,10 +1,13 @@
 // Install the assemblyai package by executing the command "npm install assemblyai"
 
-import { API_KEY } from './config.js';
+import { ASSEMBLY_API_KEY, GEMINI_API_KEY } from './config.js';
 import { AssemblyAI } from "assemblyai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const client = new AssemblyAI({
-  apiKey: API_KEY,
+  apiKey: ASSEMBLY_API_KEY,
 });
 
 // const audioFile = "./local_file.mp3";
@@ -21,6 +24,20 @@ const run = async () => {
   const transcript = await client.transcripts.transcribe(params);
 
   console.log(transcript.text);
+  console.log("Speaker Labels:", transcript.speaker_labels);
+  console.log("[END]");
+  if (transcript.utterances) {
+    for (const utterance of transcript.utterances) {
+      const speaker = utterance.speaker;
+      const text = utterance.text;
+      console.log(`Speaker ${speaker}: ${text}`);
+    }
+  } else {
+    console.log("Utterances not available. Ensure speaker_labels was set to true.");
+  }
+  const result = await model.generateContent("Summarize the following text in a concise manner, giving advice to the listener: " + transcript.text);
+  const text = result.response.text();
+  console.log(text); 
 };
 
 run();
